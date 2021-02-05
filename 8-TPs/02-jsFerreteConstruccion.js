91b5f64811223ab13a45d0b08e20a4264f049171
unknown <matiasprutscher@gmail.com>
2021-02-04 21:02:39 -0300

    Tp 1 realizado y Tp 2 falta el ultimo punto

--- 8-TPs/02-jsFerreteConstruccion.js
+++ 8-TPs/02-jsFerreteConstruccion.js
@@ -4,15 +4,66 @@ B.	mostrar la cantidad de alambre a comprar  si se ingresara el radio  de un ter
 C.	Para hacer un contrapiso de 1m x 1m se necesitan 2 bolsas de cemento y 3 de cal, debemos mostrar cuantas bolsas se necesitan de cada uno para las medidas que nos ingresen.
 */
 
-function Rectangulo () 
-{
+// txtIdLargo - txtIdAncho - txtIdRadio
 
+function Rectangulo () 
+{	
+	//Declaracion de variables
+	var largoDelTerreno;
+	var anchoDelTerreno;
+	var dimensionDelTerreno;
+	var cantidadDeAlambre;
+
+	//Toma de datos
+	largoDelTerreno = document.getElementById('txtIdLargo').value;
+	anchoDelTerreno = document.getElementById('txtIdAncho').value;
+
+	//Pasaje a caracteres a numeros
+	largoDelTerreno = parseFloat(largoDelTerreno);
+	anchoDelTerreno = parseFloat(anchoDelTerreno);
+
+	//Realizacion de calculos
+	dimensionDelTerreno = largoDelTerreno + anchoDelTerreno;
+	dimensionDelTerreno = dimensionDelTerreno * 2;
+	cantidadDeAlambre = dimensionDelTerreno * 3;
+
+	//Muestra del resultado
+	alert("La cantidad de alambre necesaria es de: " + cantidadDeAlambre + " metros");
+	
 }
 function Circulo () 
 {
-	
+	//Declaracion de variables
+	var radioDelTerreno;
+	var diametroDelTerreno
+	var cantidadDeAlambre;
+
+	//Toma de datos
+	radioDelTerreno = document.getElementById('txtIdRadio').value;
+
+	//Realizacion de calculos
+	diametroDelTerreno = radioDelTerreno * 2;
+	cantidadDeAlambre = diametroDelTerreno * 3;
+
+	//Muestra del resultado
+	alert("La cantidad de alambre necesaria es de: " + cantidadDeAlambre + " metros");
+
 }
 function Materiales () 
 {
+	//Declaracion de variables
+	var largoDelTerreno;
+	var anchoDelTerreno;
+	var metrosCuadradosDelTerreno;
+	var cantidadDeCal;
+	var cantidadDeCemento;
+
+	//Toma de datos
+	largoDelTerreno = document.getElementById('txtIdLargo').value;
+	anchoDelTerreno = document.getElementById('txtIdAncho').value;
+
+	//Realizacion de calculos
 	
+
+
 }
