19a9211e01b1b766afab422b43be50d355ce4c67
unknown <matiasprutscher@gmail.com>
2021-02-08 19:03:27 -0300

    InstruccionSwitch completada

--- 3-InstruccionSwitch/jsInstruccionSwitch-09.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-09.js
@@ -1,4 +1,7 @@
-/*una agencia de viajes debe sacar las tarifas de los viajes , se cobra $15.000 por cada estadia como base, 
+/*
+Prutscher Matias 
+1°C
+una agencia de viajes debe sacar las tarifas de los viajes , se cobra $15.000 por cada estadia como base, 
 se pide el ingreso de una estacion del año y localidad para vacacionar para poder calcular el precio final
 
 en Invierno: bariloche tiene un aumento del 20% cataratas y Cordoba tiene un descuento del 10% Mar del plata tiene un descuento del 20%
@@ -7,9 +10,78 @@ en Verano: bariloche tiene un descuento del 20% cataratas y Cordoba tiene un aum
 
 en Otoño y Primavera: bariloche tiene un aumento del 10% cataratas tiene un aumento del 10% Mar del plata tiene un aumento del 10% y
 Cordoba tiene el precio sin descuento*/
+//ID txtIdEstacion		txtIdDestino
 function mostrar()
 {
-	var estacionIngresada =txtIdEstacion.value;
-	alert(estacionIngresada);
+	//Declaracion de variables
+	var estacionIngresada; 
+	var destino;
+	var tarifa;
+	var porcentaje;
+	var precioFinal;
+
+	//Tomo datos
+	estacionIngresada =document.getElementById('txtIdEstacion').value;
+	destino = document.getElementById('txtIdDestino').value;
+
+	//Defino valor de la tarifa
+	tarifa = 15000;
+
+	//switch para determinar el porcentaje
+	switch(estacionIngresada)
+	{
+		case "Invierno":
+		case "Verano":
+			//switch define el destino
+			switch(destino)
+			{
+				case "Bariloche":
+				case "Mar del plata":
+					porcentaje = 20;
+					break;
+
+				default:
+					porcentaje = 10;
+					break;
+			}//Fin switch(destino)
+			break;
+		
+		default:
+			switch(destino)
+			{
+				case "Cordoba":
+					porcentaje = 0;
+					break;
+
+				default:
+					porcentaje = 10;
+					break;
+			}//Fin switch(destino)
+
+	}//Fin switch(estacionIngresada)
+
+	//If para corregir porcentaje negativo
+	if(estacionIngresada == "Invierno" && destino == "Mar del plata")
+	{
+
+		porcentaje = porcentaje * -1;
+
+	}//Fin if(estacionIngresada == "Invierno" && destino == "Mar del plata")
+	else
+	{
+		if(estacionIngresada == "Verano" && destino == "Bariloche")
+		{
+
+			porcentaje = porcentaje * -1;
+
+		}//Fin if(estacionIngresada == "Verano" && destino == "Bariloche")
+
+	}//Fin else if(estacionIngresada == "Invierno" && destino == "Mar del plata")
+
+	//Realizacion de calculos
+	porcentaje = tarifa * (porcentaje/100);
+	precioFinal = tarifa + porcentaje;
+
+	alert("El precio seria de: " + precioFinal);
 
 }//FIN DE LA FUNCIÓN
--- 3-InstruccionSwitch/jsInstruccionSwitch-10.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-10.js
@@ -1,6 +1,73 @@
+/*
+Prutscher Matias 
+1°C
+Enunciado:
+una agencia de viajes nos piden informar si hacemos viajes a lugares según la estación del año estemos, informar si "Se viaja" o "No se viaja" a ese lugar
+
+en Invierno: Solo Bariloche informa "se viaja" los demas destinos "No se viaja"
+
+en Verano: Se viaja a Mar del plata y Cataratas solamente
+
+en Otoño: Se viaja a todos los destinos.
+
+primavera: solo no se viaja a Bariloche*/
 function mostrar()
 {
-	var estacionIngresada =txtIdEstacion.value;
-	alert(estacionIngresada);
+	//Declaracion de variables
+	var estacionIngresada; 
+	var destino;
+
+	//Toma de datos
+	estacionIngresada =document.getElementById('txtIdEstacion').value;
+	destino = document.getElementById('txtIdDestino').value;
+
+	switch(estacionIngresada)
+	{
+		case "Invierno":
+			switch(destino)
+			{
+				case "Bariloche":
+					alert("Se viaja");
+					break;
+
+				default:
+					alert("No se viaja");
+					break;
+			}//Fin switch(destino) Invierno
+			break;
+
+		case "Verano":
+			switch(destino)
+			{
+				case "Mar del plata":
+				case "Cataratas":
+					alert("Se viaja");
+					break;
+
+				default:
+					alert("No se viaja");
+					break;
+			}//switch(destino) Verano
+			break;
+
+		case "Otoño":
+			alert("se viaja");
+			break;
+		
+		case "Primavera":
+			switch(destino)
+			{
+				case "Bariloche":
+					alert("No se viaja");
+					break;
+
+				default:
+					alert("Se viaja");
+					break;
+			}// switch(destino) Primavera
+			break;
+
+
+	}//Fin switch(estacionIngresada)
 
 }//FIN DE LA FUNCIÓN
